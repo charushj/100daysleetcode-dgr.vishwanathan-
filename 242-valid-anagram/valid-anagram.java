6f2e70480437964690f127f6f377de5bccd6ff15
Charush Jain <89185965+charushj@users.noreply.github.com>
2023-12-16 22:11:34 +0530

    Time: 4 ms (73.04%) | Memory: 44 MB (17.96%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char [] ss = s.toCharArray(), tt = t.toCharArray();
+        Arrays.sort(ss); Arrays.sort(tt);
+        return Arrays.equals(ss, tt);
+    }
+}
